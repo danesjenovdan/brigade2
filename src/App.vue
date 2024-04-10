c7062c6ed768c7968cbee654fe9a90d2000bee20
muki <filipdobranic@gmail.com>
2024-04-10 17:40:03 +0200

    initial contentful commit

---
+++ src/App.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts">
+import vSelect from 'vue-select';
+import 'vue-select/dist/vue-select.css';
+
+import TheChart from './components/TheChart.vue';
+
+const selectedUsers = defineModel()
+
+const users = [
+  { username: "nmusar", displayname: "Nataša predsednica" },
+  { username: "tfajon", displayname: "Tanja ministrica" },
+  { username: "sarecmarjan", displayname: "Marjan minister" },
+  { username: "milijonar", displayname: "Jonas poslanec" },
+  { username: "strankalevica", displayname: "Levica stranka" },
+  { username: "strankaSD", displayname: "Stranka SD" },
+  { username: "MatjaNemec", displayname: "Ni ž-ja za Matjaža" },
+  { username: "ZidanDejan", displayname: "Dejan reče Z namesto Ž" },
+]
+
+</script>
+
+<template>
+  <div class="">
+    <img src="./assets/banner.jpg" class="w-max h-auto">
+    <div class="container m-auto p-4 pb-20 text-lg">
+      <h1 class="pb-4 text-4xl">Twitter brigade 2.0</h1>
+      <p class="pb-4">Družbeno omrežje Twitter se je odkar smo januarja 2021 njegova lažna občinstva prvič preiskovali v
+        okviru
+        projekta Twittter Brigade precej spremenilo. Slovenija je v tem času dobila novo vlado, kontroverzno omrežje pa
+        novega lastnika in novo ime. Premier Golob in njegova stranka Gibanje Svoboda so omrežje kmalu po volitvah
+        protestno
+        zapustili z argumentoma, da ga pravzaprav uporablja zelo malo ljudi in da je diskurz, ki ga lahko spremljamo na
+        Twitterju ugrabljen s strani določene stranke (namig: stranke SDS).</p>
+      <p class="pb-4">Oboje drži - da Twitter uporablja pravzaprav precej malo Slovencev je prvi opozoril podatkovni
+        analitik
+        Marko Plahuta s katerim sodelujemo tudi pri tokratnji raziskavi - po njegovih ugotovitvah je namreč na
+        slovenskem
+        delu Twitterja aktivnih zgolj 7000 uporabnikov, kar znaša XX procentov populacije. Za primerjavo, na Instagramu
+        naj
+        bi bilo XX slovencev, na Facebooku XXX. Vendar ta ocena zajema zgolj aktivne uporabnike - koliko je še takšnih,
+        ki
+        omrežje redno spremljajo, je težje oceniti. Prav tako je za doseg Twitterja pomembno razumeti njegov učinek na
+        širši
+        medijski prostor, saj tradicionalni mediji tvite politikov in mnenjskih voditeljev redno povzemajo in obravnajo
+        kot
+        novodobne izjave za javnost. Še več - mediji pogosto poročajo tudi o besednih spopadih med znanimi uporabniki
+        ali
+        kontroverznih ritvitih in s tem utrjujejo vlogo omrežje kot de facto “mestnega trga” (town square).</p>
+      <p class="pb-4">V letu 2023 smo pripravljali nadaljevanje naše odmevne raziskave Twitter Brigade, ki je razkrila
+        in
+        izpostavila številne lažne profile. Raziskavo smo želeli nadgraditi z novimi metodami analiz, ki bi ponudile
+        globlji
+        analitični vpogled v manipuliranje z lažnimi profili in umetno ustvarjanje kredibilnosti in teže posameznih
+        profilov. Žal pa zaradi novo vzpostavljenih omejitev uporabe programskega vmesnika (API) omrežja X, ki jih je
+        uvedel
+        njegov lastnik Elon Musk, analiz ni več mogoče izvesti in replicirati na način, kot smo ga predvideli.</p>
+      <p class="pb-4">Kljub temu, da je omrežje X pravzaprav v zatonu, in da se na srečo vzpostavljajo bolj digitalno
+        suverene
+        alternative (link na mastodon), vseeno objavljamo eno od analiz, ki smo jih pripravili, za katero menimo, da bi
+        lahko bila zanima za splošno javnost in raziskovalce družabnih omrežji.</p>
+      <h2 class="pb-4 text-4xl">Analiza: Čas sledenja</h2>
+      <p class="pb-4">Število sledilcev je ključen podatek o vplivu oziroma pomembnosti Twitter/X profila. Več
+        sledilcev,
+        kot
+        ima profil, večji je videz priljubljenosti oziroma zaupanja javnosti. Deluje kot nekakšna kvantifikacija
+        kredibilnosti ali ugleda in je eden od najbolj pomembnih indikatorjev na družbenih omrežjih.</p>
+      <p class="pb-4">Podatek o tem, kdaj je posamezni uporabnik posledil drugega uporabnika ni javno znan. Je pa
+        pomemben
+        za
+        razumevanje širšega konteksta, kdaj je posamezen profil pridobil pozornost javnosti (in s tem nove sledilce).
+      </p>
+      <p class="pb-4">Twitter/X predstavlja seznam sledilcev posameznega profila v kronološkem zaporedju. Tisti profili,
+        ki
+        so
+        bili posledeni najprej, so na dnu, tisti, ki so bili nazadnje, so na vrhu. To dejstvo, v kombinaciji z javno
+        dostopnim podatkom, kdaj je bil kateri profil ustvarjen, nam omogoča, da lahko relativno natančno ocenimo, kdaj
+        je
+        določen profil posledil drugega.</p>
+      <p class="pb-4">METODOLOGIJA ŠE BOLJ PODROBNO MOGOČE CELO SNIPPET FORMULE / KODE?</p>
+
+      <p class="font-bold">Primer: distribucija tipičnega uporabnika</p>
+      <p class="pb-4">Število sledilcev raste linearno skozi čas in je neodvisno od političnih dogodkov.</p>
+      <div class="m-auto overflow-x-auto">
+        <TheChart selected_username="nmusar" />
+      </div>
+      <p class="font-bold">Primer: distribucija atipičnega uporabnika</p>
+      <p class="pb-4">Število sledilcev poskoči v določenih obdobjih.</p>
+      <div class="m-auto overflow-x-auto">
+        <TheChart selected_username="tfajon" />
+      </div>
+
+      <p class="pb-4">Na podlagi opisane metodologije smo pripravili izračun časovne distribucije sledilcev za 100
+        slovenskih profilov z visokim številom sledilcev. Izbrali smo 20 politikov iz 20 vplivnežev iz vsakega
+        političnega pola*, ki imajo največ sledilcev.</p>
+      <p class="">Njihove distribucije si lahko ogledate pregledovalniku.</p>
+      <v-select multiple :options="users" label="displayname" v-model="selectedUsers"></v-select>
+      <div class="m-auto overflow-x-auto" v-for="selectedUser in selectedUsers">
+        <p class="font-bold">{{ selectedUser.displayname }} ({{ selectedUser.username }})</p>
+        <TheChart :selected_username="selectedUser.username.toLowerCase()" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/TheChart.vue
@@ -0,0 +1,282 @@
+<script setup lang="ts">
+import { ref, onMounted, onUpdated } from 'vue'
+
+import * as d3 from 'd3';
+
+const props = defineProps<{ selected_username: string }>()
+
+const divRef = ref(null);
+
+const vert_annotations = [
+    { date: new Date(2020, 2, 20), text: "Janševa vlada", pres: "Janša", text_date: "13.3.20" }, // color: "#edafab", text_color: "#ff0000"
+    //{date: new Date(2012, 1, 10), text: "Janševa vlada", text_date: "10-Feb-2012", color: "#edafab", text_color: "#ff0000"},
+    { date: new Date(2022, 3, 24), text: "Vol 22", pres: "Golob", text_date: "24.4.22" },
+    { date: new Date(2008, 8, 29), text: "Vol 08", pres: "Pahor", text_date: "29.9.08" },
+    { date: new Date(2011, 11, 4), text: "Vol 11", pres: "Janša", text_date: "4.12.11" },
+    { date: new Date(2014, 6, 13), text: "Vol 14", pres: "Cerar", text_date: "13.7.14" },
+    { date: new Date(2018, 5, 3), text: "Vol 18", pres: "Šarec", text_date: "3.6.18" },
+    { date: new Date(2013, 2, 20), text: "Bratušek", pres: "Bratušek", text_date: "20.3.13" },
+    { date: new Date(2017, 9, 22), text: "Vol PR", pres: "Pahor", text_date: "22.10.17" },
+    { date: new Date(2016, 10, 8), text: "Vol 16", pres: "ZDA Trump", text_date: "8.11.16" },
+    { date: new Date(2015, 3, 1), text: "Kriza", pres: "Cerar", text_date: "1.4.15" },
+    //{date: new Date(2018, 9, 22), text: "Preds. vol. 2017", text_date: "22-Oct-2018"}
+]
+
+async function follower_dist_grid(username: string) {
+    let min_date = new Date("2023"), max_date = new Date("2000");
+
+    const order_response = await fetch(`./${username}_order.json`);
+    const username_order = await order_response.json();
+
+    const cumulative_response = await fetch(`./${username}_cumulative.json`);
+    const username_cumulative = await cumulative_response.json();
+
+    const registration_response = await fetch(`./${username}_registration.json`);
+    const username_registration = await registration_response.json();
+
+    const old_vs_new = [];
+    const orientation = [];
+    const ghosts = [];
+
+    let min_val = 10000
+    let max_val = 0;
+
+    let data = username_order.values
+        .map((d, i: number) => {
+            old_vs_new.push({ x: new Date(d.x), y: d.t.percent_new })
+            orientation.push({ x: new Date(d.x), r: d.o.r, l: d.o.l })
+            ghosts.push({ x: new Date(d.x), ns: d.no_statuses, nf: d.no_followers })
+            d.t.values.forEach(k => {
+                k.extent = d3.extent(d.t.values, d => d.y)
+                k.idx = i;
+                k.time_idx = new Date(d.x)
+                let date = new Date(k.x)
+                k.x = date
+                if (date > max_date) max_date = date;
+                if (date < min_date) min_date = date;
+                if (k.y > max_val) max_val = k.y;
+                if (k.y < min_val) min_val = k.y;
+
+            })
+            return {
+                time: new Date(d.x),
+                values: d.t.values
+            }
+        })
+
+    const horiz_end = 800
+    // const horiz_range = [200, horiz_end]
+    const horiz_range = [0, horiz_end]
+    const horiz_range_span = horiz_range[1] - horiz_range[0];
+    let time_scale = d3.scaleLinear().range([900, 300]);
+    let y_val_scale = d3.scaleLinear().range([0, 80]);
+    let y_cumul_scale = d3.scaleLinear().range([0, 80]);
+    let y_registration_scale = d3.scaleLinear().range([0, 80]);
+    let y_perc_new_scale = d3.scaleLinear().range([0, 80]);
+
+
+    let horiz_time_scale = d3.scaleLinear().range(horiz_range);
+    const followers = username_order.values;
+
+    time_scale.domain([min_date, max_date])
+
+    horiz_time_scale.domain(d3.extent(data, d => d.time))
+    y_val_scale.domain(d3.extent(followers, d => d.y))
+    y_registration_scale.domain(d3.extent(username_registration.values, d => d.y))
+    y_perc_new_scale.domain([0, 1])
+    y_cumul_scale.domain(d3.extent(username_cumulative.values, d => d.y))
+
+    let div = d3.select(divRef.value);
+    div.selectAll("*").remove()
+    let svg = div.append("svg")
+        .attr("class", "m-auto mt-4 mb-4")
+        .attr("width", 900)
+        .attr("height", 250)
+
+    let grid = svg.selectAll("g").data(data).enter();
+    let row = grid.append("g");
+
+    let cumulative_chart = svg.selectAll("rect.a").data(username_cumulative.values).enter()
+    cumulative_chart
+        .append("rect")
+        .attr("x", (d, i) => {
+            return horiz_time_scale(d.x)
+        })
+        .attr("y", d => 100 - y_cumul_scale(d.y))
+        .attr("width", horiz_range_span / username_cumulative.values.length)
+        .attr("height", d => y_cumul_scale(d.y))
+        .style("stroke", "white")
+        .style("stroke-width", 0.5)
+        .style("fill", "#9dc6f5")
+    // .on("mouseover", d => console.log(new Date(d.x), d))
+
+    svg.append("text")
+        .attr("x", horiz_range[1] + 10)
+        .attr("y", 20)
+        .text(("" + y_cumul_scale.domain()[1]).slice(0, 5))
+        .style("font-size", "0.6em")
+        .style("fill", "darkblue")
+
+    svg.append("text")
+        .attr("x", horiz_range[1] + 10)
+        .attr("y", 100)
+        .text(0)
+        .style("font-size", "0.6em")
+        .style("fill", "darkblue")
+
+    let follower_increase_chart = svg.selectAll("rect.a").data(followers).enter()
+    follower_increase_chart
+        .append("rect")
+        .attr("x", (d, i) => {
+            return horiz_time_scale(d.x)
+        })
+        .attr("y", d => 200 - y_val_scale(d.y))
+        .attr("width", horiz_range_span / followers.length)
+        .attr("height", d => y_val_scale(d.y))
+        .style("stroke", "white")
+        .style("stroke-width", 0.5)
+        .style("fill", "#f21347")
+
+    svg.append("text")
+        .attr("x", horiz_range[1] + 10)
+        .attr("y", 120)
+        .text(("" + y_val_scale.domain()[1]).slice(0, 5))
+        .style("font-size", "0.6em")
+        .style("fill", "#f21347")
+
+    svg.append("text")
+        .attr("x", horiz_range[1] + 10)
+        .attr("y", 200)
+        .text(0)
+        .style("font-size", "0.6em")
+        .style("fill", "#f21347")
+
+
+
+
+    vert_annotations
+        .forEach((d, idx) => {
+
+            let x_coord = horiz_time_scale(d.date)
+            if (d.date > d3.extent(data, d => d.time)[0]) {
+                let line_gen = d3.line()
+
+                const points = [
+                    [x_coord, 0],
+                    [x_coord, 250]
+                ]
+                let line_path = line_gen(points);
+                svg.append('path')
+                    .attr('d', line_path)
+                    .style("stroke", d.color !== undefined ? d.color : "#999")
+                    .style("stroke-width", 2.5)
+                    .style("stroke-dasharray", "1 5");
+
+                svg.append("text")
+                    .attr("y", 8)
+                    .attr("x", x_coord + 4)
+                    .text(d.text)
+                    .style("fill", d.color !== undefined ? d.text_color : "#999")
+                    .style("font-size", "0.6em")
+                    .attr("text-anchor", "start")
+                svg.append("text")
+                    .attr("y", 18)
+                    .attr("x", x_coord + 4)
+                    .text(d.text_date)
+                    .style("fill", d.color !== undefined ? d.text_color : "#999")
+                    .style("font-size", "0.6em")
+                    .attr("text-anchor", "start")
+                svg.append("text")
+                    .attr("y", 28)
+                    .attr("x", x_coord + 4)
+                    .text(d.pres)
+                    .style("fill", d.color !== undefined ? d.text_color : "#000")
+                    .style("font-size", "0.6em")
+                    .attr("text-anchor", "start")
+
+                svg.append("text")
+                    .attr("y", 2078)
+                    .attr("x", x_coord + 4)
+                    .text(d.text)
+                    .style("fill", d.color !== undefined ? d.text_color : "#999")
+                    .style("font-size", "0.6em")
+                    .attr("text-anchor", "start")
+                svg.append("text")
+                    .attr("y", 2088)
+                    .attr("x", x_coord + 4)
+                    .text(d.text_date)
+                    .style("fill", d.color !== undefined ? d.text_color : "#999")
+                    .style("font-size", "0.6em")
+                    .attr("text-anchor", "start")
+            }
+
+        })
+
+    svg.append("text")
+        .attr("y", 35)
+        .attr("x", 10)
+        .text("Kumulativno število")
+        .style("fill", "#358df2")
+        .style("font-size", "0.6em")
+        .style("font-family", "Courier New, monospace")
+        .attr("text-anchor", "start")
+    svg.append("text")
+        .attr("y", 45)
+        .attr("x", 10)
+        .text("sledilcev")
+        .style("fill", "#358df2")
+        .style("font-size", "0.6em")
+        .style("font-family", "Courier New, monospace")
+        .attr("text-anchor", "start")
+
+    svg.append("text")
+        .attr("y", 135)
+        .attr("x", 10)
+        .text("Ocenjen mesečni")
+        .style("fill", "#f21347")
+        .style("font-size", "0.6em")
+        .style("font-family", "Courier New, monospace")
+        .attr("text-anchor", "start")
+    svg.append("text")
+        .attr("y", 145)
+        .attr("x", 10)
+        .text("prirast sledilcev")
+        .style("fill", "#f21347")
+        .style("font-size", "0.6em")
+        .style("font-family", "Courier New, monospace")
+        .attr("text-anchor", "start")
+    svg.append("text")
+        .attr("y", 155)
+        .attr("x", 10)
+        .text("(v katerem obdobju")
+        .style("fill", "#f21347")
+        .style("font-size", "0.6em")
+        .style("font-family", "Courier New, monospace")
+        .attr("text-anchor", "start")
+    svg.append("text")
+        .attr("y", 165)
+        .attr("x", 10)
+        .text("je dobil največ sledilcev)")
+        .style("fill", "#f21347")
+        .style("font-size", "0.6em")
+        .style("font-family", "Courier New, monospace")
+        .attr("text-anchor", "start")
+
+}
+
+onMounted(() => {
+    follower_dist_grid(props.selected_username);
+});
+
+onUpdated(() => {
+    follower_dist_grid(props.selected_username);
+})
+
+</script>
+
+<template>
+    <div class="" ref="divRef">
+    </div>
+</template>
+
+<style scoped></style>
